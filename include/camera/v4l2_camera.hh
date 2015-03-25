4bc343f76c12e47b71d34b76a9786bbdf7f8b09f
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-03-25 23:14:03 +0100

    V4L2 based camera module, providing YUYV format.

---
+++ include/camera/v4l2_camera.hh
@@ -0,0 +1,212 @@
+/*
+Tinkervision - Vision Library for https://github.com/Tinkerforge/red-brick
+Copyright (C) 2015 philipp.kroos@fh-bielefeld.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#ifndef V4L2_CAMERA_H
+#define V4L2_CAMERA_H
+
+#include <array>
+
+#include <fcntl.h>
+#include <cerrno>
+#include <cstring>       // std::strerror
+#include <system_error>  // std::errc
+
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+
+#include <linux/videodev2.h>
+#include <libv4l2.h>
+
+// stat, for device_exists()
+#include <sys/stat.h>
+
+// baseclass
+#include "camera.hh"
+
+// aliases for v4l2 types and functions plus definition of related types
+namespace v4l2 {
+
+// v4l2 types
+using Requestbuffers = v4l2_requestbuffers;
+using Buffer = v4l2_buffer;
+using BufferType = v4l2_buf_type;
+using BufferMemory = v4l2_memory;
+using Format = v4l2_format;
+using PixelFormat = v4l2_pix_format;
+using FrameIntervalEnum = v4l2_frmivalenum;
+using StreamParameter = v4l2_streamparm;
+
+// additional types
+using Frame = struct Frame {
+    void* start = nullptr;
+    size_t length = 0;
+    bool mapped = false;  // preventing unmap of unmapped
+};
+
+// constants needed in the V4L2USBCamera declaration
+static v4l2_buf_type BUFFER_TYPE_VIDEO_CAPTURE = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+static auto BUFFER_MEMORY_MMAP = V4L2_MEMORY_MMAP;
+// static auto YV12 = V4L2_PIX_FMT_YVU420;  // planar. Encoder-Accepted.
+static auto YUYV = V4L2_PIX_FMT_YUYV;  // 422, packed
+
+struct Request {
+    long unsigned const value;
+    char const* name;
+};
+
+// Functor wrapping ioctl requests.  If evaluation yields false, the
+// occured error can be retrieved from result until the next
+// evaluation. It's the system errno, see std::errc and std::strerror)
+class IOControl {
+    inline bool _error_during_ioctl(void) const { return result == -1; }
+    inline bool _interrupted(void) const { return errno == EINTR; }
+    inline bool _unavailable(void) const { return errno == EAGAIN; }
+
+public:
+    bool operator()(int device_handle, Request const& request, void* arg) {
+
+        do {
+            result = v4l2_ioctl(device_handle, request.value, arg);
+        } while (_error_during_ioctl() and (_interrupted() or _unavailable()));
+
+        if (_error_during_ioctl()) {
+
+            // Store a meaningfull error number for the caller
+            result = errno;
+        } else {
+            result = 0;
+        }
+
+        return result == 0;
+    }
+
+    int result = 0;
+};
+}
+
+namespace tfv {
+
+using frame_resolution = struct frame_resolution {
+    const char* format;
+    size_t width;
+    size_t height;
+};
+
+using ImageFormatMapping = struct ImageFormatMapping {
+    const unsigned v4l2_id;
+    const tfv::ImageFormat tfv_id;
+};
+
+class V4L2USBCamera : public Camera {
+public:
+    V4L2USBCamera(TFV_Id camera_id);
+    virtual ~V4L2USBCamera(void);
+
+    virtual bool open(void);
+    virtual bool is_open(void) const;
+    virtual ImageFormat image_format(void) const {
+        return supported_codings_[coding_].tfv_id;
+    }
+
+    bool select_best_available_settings(void);
+
+protected:
+    virtual bool retrieve_frame(tfv::Image& image);
+    virtual void retrieve_properties(size_t& height, size_t& width,
+                                     size_t& frame_bytesize);
+    virtual void close(void);
+
+private:
+    std::array<ImageFormatMapping, 1> supported_codings_ = {{
+        {v4l2::YUYV, ImageFormat::YUYV},
+        // Got no hw supporting this to test it yet
+        //{v4l2::YV12, ImageFormat::YV12},
+    }};
+
+    std::array<frame_resolution, 5> supported_resolutions_ = {{
+        {"WUXGA", 1920, 1200},   // 16:10
+        {"FullHD", 1920, 1080},  // 16:9, 1080p
+        {"HDTV", 1280, 720},     // 16:9, 720p
+        {"LD", 640, 480},        // 4:3
+        {"VHS", 320, 240},       // 4:3
+    }};
+
+    int device_ = 0;  ///< camera device handle
+
+    static const int request_buffer_count_ =
+        4;  ///< Frame buffers provided to v4l
+
+    v4l2::Requestbuffers request_buffers_;
+
+    v4l2::BufferType buffer_type_ =
+        v4l2::BUFFER_TYPE_VIDEO_CAPTURE;  ///< recording a stream of images
+
+    v4l2::BufferMemory buffer_memory_ =
+        v4l2::BUFFER_MEMORY_MMAP;  ///< exchanging data with the driver via
+    /// memory-mapped buffers
+
+    v4l2::Buffer buffer_;  ///< 'Working' buffer; pointing to 'real' buffer
+    v4l2::Frame* frames_ = nullptr;  ///< memory-mapped actual buffer
+
+    size_t frame_width_ = 0;     ///< resolution width
+    size_t frame_height_ = 0;    ///< resolution height
+    size_t frame_bytesize_ = 0;  ///< size of data retrieved per frame
+
+    int coding_ = -1;       ///< index into supported_codings_
+    int resolution_ = -1;   ///< index into supported_resolutions_
+    double framerate_ = 0;  ///< not settable currently
+    bool running_ = false;  ///< True if capturing frames
+
+    // helper
+    bool _start_capturing(void);
+    void _close_device(void);
+    int _capture_frame_byte_size(void);
+    bool _init_request_buffers(void);
+    inline void _init_info_buffer(int index);
+    bool _set_best_format_and_resolution(v4l2::Format& format);
+    bool _set_highest_framerate(v4l2::PixelFormat& px_format);
+    void _retrieve_properties(void);
+
+    v4l2::IOControl io_control_;  ///< system ioctl abstraction
+
+    // io_control_ wrapper
+    bool io_operation(int const& device_handle, v4l2::Request const& request,
+                      void* arg, int ignorable) {
+
+        auto result = io_control_(device_handle, request, arg);
+        if (not result) {
+            auto err = io_control_.result;
+            std::cout << "Error " << strerror(err) << "(" << err
+                      << "), which is "
+                      << (ignorable == err ? "ignorable" : "not ignorable")
+                      << " for request " << request.name << std::endl;
+        }
+        return result;
+    }
+
+    bool io_operation(int const& device_handle, v4l2::Request const& request,
+                      void* arg) {
+
+        return io_operation(device_handle, request, arg, 0);
+    }
+};
+}
+#endif
---
+++ src/camera/v4l2_camera.cc
@@ -0,0 +1,410 @@
+/*
+Tinkervision - Vision Library for https://github.com/Tinkerforge/red-brick
+Copyright (C) 2014-2015 philipp.kroos@fh-bielefeld.de
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+#include <cstring>  // memset
+
+// cam access
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+#include <linux/videodev2.h>
+#include <libv4l2.h>
+
+#include "v4l2_camera.hh"
+
+namespace v4l2 {
+
+static auto TIME_PER_FRAME = V4L2_CAP_TIMEPERFRAME;
+static auto DISCRETE_INTERVAL = V4L2_FRMIVAL_TYPE_DISCRETE;
+static auto PROGRESSIVE = V4L2_FIELD_NONE;
+
+// errors
+static auto INVALID_VALUE = EINVAL;
+
+static Request get_parameter = {VIDIOC_G_PARM, "'get parameter'"};
+static Request set_parameter = {VIDIOC_S_PARM, "'set parameter'"};
+static Request get_format = {VIDIOC_G_FMT, "'get format'"};
+static Request set_format = {VIDIOC_S_FMT, "'set format'"};
+static Request enumerate_frameintervals = {VIDIOC_ENUM_FRAMEINTERVALS,
+                                           "'enumerate frameintervals'"};
+static Request request_buffers = {VIDIOC_REQBUFS, "'request buffers'"};
+static Request deque_buffers = {VIDIOC_DQBUF, "'deque buffers'"};
+static Request queue_buffers = {VIDIOC_QBUF, "'queue buffers'"};
+static Request stream_on = {VIDIOC_STREAMON, "'stream on'"};
+static Request stream_off = {VIDIOC_STREAMOFF, "'stream off'"};
+static Request query_buffers = {VIDIOC_QUERYBUF, "'query buffers'"};
+
+// functions
+static auto mmap = v4l2_mmap;
+static auto munmap = v4l2_munmap;
+static auto open = v4l2_open;
+static auto close = v4l2_close;
+}
+
+tfv::V4L2USBCamera::V4L2USBCamera(TFV_Id camera_id) : Camera(camera_id) {
+    // zero-initialize buffers for the frames to be grabbed
+    frames_ = new v4l2::Frame[request_buffer_count_ * sizeof(v4l2::Frame)]();
+}
+
+tfv::V4L2USBCamera::~V4L2USBCamera(void) { _close_device(); }
+
+bool tfv::V4L2USBCamera::is_open(void) const { return device_ != 0; }
+
+bool tfv::V4L2USBCamera::open(void) {
+    if (is_open()) {
+        return true;
+    }
+
+    const char* device =
+        (std::string("/dev/video") + std::to_string(camera_id_)).c_str();
+
+    device_ = v4l2::open(device, O_RDWR | O_NONBLOCK, 0);
+
+    if (device_ == -1) {
+        device_ = 0;
+    }
+
+    if (device_) {
+        if (not select_best_available_settings() or (not _start_capturing())) {
+            close();
+        }
+    }
+
+    return is_open();
+}
+
+void tfv::V4L2USBCamera::retrieve_properties(size_t& framewidth,
+                                             size_t& frameheight,
+                                             size_t& frame_bytesize) {
+
+    if (not frame_width_ and is_open()) {
+        _retrieve_properties();
+    }
+
+    framewidth = frame_width_;
+    frameheight = frame_height_;
+    frame_bytesize = frame_bytesize_;
+}
+
+void tfv::V4L2USBCamera::_retrieve_properties(void) {
+
+    if (not frame_width_ and is_open()) {
+        v4l2::Format format;
+
+        format.type = buffer_type_;
+
+        auto result = io_operation(device_, v4l2::get_format, &format);
+        if (result) {
+            frame_width_ = format.fmt.pix.width;
+            frame_height_ = format.fmt.pix.height;
+            frame_bytesize_ =
+                format.fmt.pix.bytesperline * format.fmt.pix.height;
+        }
+    }
+}
+
+bool tfv::V4L2USBCamera::select_best_available_settings(void) {
+    auto result = false;
+
+    if (is_open()) {  // Todo: errornumber?
+
+        v4l2::Format format;
+        format.type = buffer_type_;
+
+        // priority 1,2,3: pixelformat, resolution, fps
+        if (not _set_best_format_and_resolution(format)) {
+
+            // found no resolution at all
+            coding_ = resolution_ = -1;
+            _close_device();
+
+        } else {
+
+            result = true;
+            (void)_set_highest_framerate(format.fmt.pix);
+        }
+    }
+    return result;
+}
+
+bool tfv::V4L2USBCamera::_set_best_format_and_resolution(v4l2::Format& format) {
+
+    auto& px_format = format.fmt.pix;
+
+    // Using the best available, top-down approach
+    // priority 1: pixelformat
+    // priority 2: resolution
+    coding_ = -1;
+    resolution_ = -1;
+    for (size_t i = 0; i < supported_resolutions_.size(); i++) {
+        for (size_t j = 0; j < supported_codings_.size(); j++) {
+            px_format.width = supported_resolutions_[i].width;
+            px_format.height = supported_resolutions_[i].height;
+            px_format.pixelformat = supported_codings_[j].v4l2_id;
+            px_format.field = v4l2::PROGRESSIVE;
+            px_format.bytesperline = 0;  // lets the driver set it
+            if (io_operation(device_, v4l2::set_format, &format)) {
+
+                coding_ = j;
+                break;
+            }
+        }
+        if (coding_ != -1) {
+
+            // driver can have set different format than requested
+            if (px_format.width == supported_resolutions_[i].width and
+                px_format.height == supported_resolutions_[i].height) {
+
+                resolution_ = i;
+                break;
+            } else {
+                coding_ = -1;
+            }
+        }
+    }
+
+    return resolution_ != -1 and coding_ != -1;
+}
+
+bool tfv::V4L2USBCamera::_set_highest_framerate(v4l2::PixelFormat& px_format) {
+    // Assumes already selected framesize
+
+    // if not supported by device, just use current setting
+    v4l2::FrameIntervalEnum frame_interval;
+
+    frame_interval.pixel_format = px_format.pixelformat;
+    frame_interval.width = px_format.width;
+    frame_interval.height = px_format.height;
+
+    auto result = false;
+
+    int max_numerator = 0;
+    int max_denominator = 1;  // prevent /0
+    framerate_ = 0.0d;
+
+    // enumerate possibilities
+    for (int i = 0;; i++) {
+
+        frame_interval.index = i;
+
+        // ignore error message for invalid value which will be returned
+        // eventually
+        result = io_operation(device_, v4l2::enumerate_frameintervals,
+                              &frame_interval, v4l2::INVALID_VALUE);
+
+        if (not result) {
+            break;
+        }
+
+        if (frame_interval.type != v4l2::DISCRETE_INTERVAL) {
+            // Only discrete framerates supported
+            continue;
+        }
+
+        auto const& discrete = frame_interval.discrete;
+        auto const fps = discrete.denominator / (double)discrete.numerator;
+
+        if (fps > framerate_) {
+            max_numerator = discrete.numerator;
+            max_denominator = discrete.denominator;
+            framerate_ = fps;
+        }
+    }
+
+    v4l2::StreamParameter stream_parameter;
+    stream_parameter.type = v4l2::BUFFER_TYPE_VIDEO_CAPTURE;
+
+    if (io_operation(device_, v4l2::get_parameter, &stream_parameter) and
+        (stream_parameter.parm.capture.capability & v4l2::TIME_PER_FRAME)) {
+
+        stream_parameter.parm.capture.timeperframe.numerator = max_numerator;
+        stream_parameter.parm.capture.timeperframe.denominator =
+            max_denominator;
+
+        // return result of trying to set the framerate
+        result = io_operation(device_, v4l2::set_parameter, &stream_parameter);
+    }
+
+    else {  // return can't set the framerate explicitly
+
+        result = false;
+    }
+
+    return result;
+}
+
+void tfv::V4L2USBCamera::close(void) {
+    if (device_) {
+        _close_device();
+    }
+
+    // deque all buffers
+    while (io_operation(device_, v4l2::deque_buffers, &buffer_))
+        ;
+
+    // un-mmap buffers
+    if (frames_) {
+        for (size_t i = 0; i < request_buffers_.count; ++i) {
+            v4l2::munmap(frames_[i].start, frames_[i].length);
+            frames_[i].mapped = false;
+        }
+    }
+
+    // cleanup and initializing
+    device_ = 0;
+    delete[] frames_;
+    frames_ = nullptr;
+    running_ = false;
+}
+
+bool tfv::V4L2USBCamera::retrieve_frame(tfv::Image& frame) {
+    auto result = false;
+    static const int timeout_secs = 1;
+    static const int timeout_usecs = 0;
+    static struct timeval tv = {timeout_secs, timeout_usecs};
+
+    fd_set fds;
+    FD_ZERO(&fds);
+    FD_SET(device_, &fds);
+
+    // device ready?
+    result = (select(device_ + 1, &fds, NULL, NULL, &tv) != -1);
+
+    if (result) {
+        // Queue last read buffer
+        if (running_) {  // start with a dequeuing
+            result = io_operation(device_, v4l2::queue_buffers, &buffer_);
+        } else {
+            running_ = true;
+        }
+    }
+
+    if (result) {
+        _init_info_buffer(0);  // index does not matter here as being set next
+
+        // retrieve frame
+        result = io_operation(device_, v4l2::deque_buffers, &buffer_);
+    }
+
+    if (result) {
+        if (not frame_bytesize_) {
+            _retrieve_properties();
+        }
+        frame.data = static_cast<TFV_ImageData*>(frames_[buffer_.index].start);
+        frame.bytesize = frame_bytesize_;
+        frame.width = frame_width_;
+        frame.height = frame_height_;
+    }
+
+    return result;
+}
+
+bool tfv::V4L2USBCamera::_start_capturing(void) {
+    auto result = false;
+
+    if (is_open() and _init_request_buffers()) {
+        result = true;
+
+        // Initialize (map) framebuffers
+        for (size_t i = 0; i < request_buffers_.count; ++i) {
+
+            auto& frame = frames_[i];
+            _init_info_buffer(i);
+
+            result = io_operation(device_, v4l2::query_buffers, &buffer_);
+            if (not result) {
+                break;
+            }
+
+            frame.length = buffer_.length;
+            frame.start =
+                v4l2::mmap(nullptr, buffer_.length, PROT_READ | PROT_WRITE,
+                           MAP_SHARED, device_, buffer_.m.offset);
+            frame.mapped = true;
+
+            if (MAP_FAILED == frame.start) {
+                // Error mmapping buffers
+                // see in errno
+                result = false;
+                break;
+            }
+        }
+
+        if (result) {
+            // queue all buffers for data exchange with driver
+            for (size_t i = 0; i < request_buffers_.count; ++i) {
+                _init_info_buffer(i);
+
+                result = io_operation(device_, v4l2::queue_buffers, &buffer_);
+                if (not result) {
+                    break;
+                }
+            }
+        }
+
+        // begin capturing
+        if (result) {
+            _init_info_buffer(0);
+            result = io_operation(device_, v4l2::stream_on, &buffer_type_);
+        }
+    }
+
+    return result;
+}
+
+void tfv::V4L2USBCamera::_close_device(void) {
+    if (is_open()) {
+        if (device_) {
+            (void)io_operation(device_, v4l2::stream_off, &buffer_type_);
+            // ignore for now
+        }
+
+        v4l2::close(device_);
+        device_ = 0;
+    }
+}
+
+bool tfv::V4L2USBCamera::_init_request_buffers(void) {
+    std::memset(&request_buffers_, 0, sizeof(request_buffers_));
+    request_buffers_.count = request_buffer_count_;
+    request_buffers_.type = buffer_type_;
+    request_buffers_.memory = buffer_memory_;
+    return io_operation(device_, v4l2::request_buffers, &request_buffers_);
+}
+
+inline void tfv::V4L2USBCamera::_init_info_buffer(int index) {
+    std::memset(&buffer_, 0, sizeof(v4l2::Buffer));
+    buffer_.type = buffer_type_;
+    buffer_.memory = buffer_memory_;
+    buffer_.index = index;
+}
+
+int tfv::V4L2USBCamera::_capture_frame_byte_size(void) {
+    v4l2::Format format;
+
+    format.type = buffer_type_;
+    if (not io_operation(device_, v4l2::get_format, &format)) {
+        return -1;
+    }
+
+    return format.fmt.pix.bytesperline * format.fmt.pix.height;
+}
