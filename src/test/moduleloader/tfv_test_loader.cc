bc8ba8cdbfc5b4fbfb7a6f2b7bb71047a58c605a
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-09-30 14:39:19 +0200

    Test unit for module_loader.

---
+++ src/test/moduleloader/tfv_test_loader.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+#include "module_loader.hh"
+
+int main() {
+    auto loader = tfv::ModuleLoader("/usr/local/lib/tinkervision",
+                                    "/tmp/lib/tinkervision/");
+    auto contents = std::vector<std::string>();
+
+    loader.list_available_modules(contents);
+    for (auto const& file : contents) {
+        std::cout << file << std::endl;
+    }
+
+    return 0;
+}
