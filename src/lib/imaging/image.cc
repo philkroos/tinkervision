7299693269536c30e1064fc06bf013a0614ed272
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-10-30 15:08:38 +0100

    Comments for image-structs.

--- src/lib/imaging/image.cc
+++ src/lib/imaging/image.cc
@@ -1,21 +1,28 @@
-/*
-Tinkervision - Vision Library for https://github.com/Tinkerforge/red-brick
-Copyright (C) 2014-2015 philipp.kroos@fh-bielefeld.de
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+/// \file image.cc
+/// \author philipp.kroos@fh-bielefeld.de
+/// \date 2015
+///
+/// \brief Implementation of image.hh declarations.
+///
+/// This file is part of Tinkervision - Vision Library for Tinkerforge Redbrick
+/// \sa https://github.com/Tinkerforge/red-brick
+///
+/// \copyright
+///
+/// This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License
+/// as published by the Free Software Foundation; either version 2
+/// of the License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+/// USA.
 
 #include "image.hh"
 
@@ -59,11 +66,15 @@ bool tv::ImageAllocator::allocate(uint16_t width, uint16_t height,
         return false;
     }
 
-    // only reallocate memory if necessary
+    /// The currently managed image will be free'd IFF the requested bytesize
+    /// differs from the currently allocated or the image manages it's own data
+    /// currently.
     if (not foreign_data or bytesize != image_init_bytesize_) {
         _free_image();
     }
 
+    /// A data block for a new image will be then allocated if foreign_data is
+    /// false.
     if (not image_.data) {
         image_init_bytesize_ = bytesize;
         if (not foreign_data) {
@@ -75,6 +86,7 @@ bool tv::ImageAllocator::allocate(uint16_t width, uint16_t height,
 
     using_foreign_data_ = foreign_data;
 
+    /// The header of image_ is initialized properly.
     image_.header.bytesize = bytesize;
     image_.header.width = width;
     image_.header.height = height;
@@ -86,31 +98,41 @@ bool tv::ImageAllocator::allocate(uint16_t width, uint16_t height,
 void tv::ImageAllocator::set_from_image(Image const& image) {
     LogDebug("IMAGE_ALLOCATOR", "Set for ", id_);
 
+    /// The currently managed image is free'd
     _free_image();
 
+    /// and image_ is set to image, creating a shallow copy.
     image_ = image;
+
+    /// After this, the data pointed to by image_ must not be deleted.
     using_foreign_data_ = true;
 }
 
 void tv::ImageAllocator::copy_data(ImageData const* data, size_t size) {
     LogDebug("IMAGE_ALLOCATOR", "Copy for ", id_);
 
+    /// This currently assumes that the method is only called for instances that
+    /// are already set to be managing their own data.
     assert(not using_foreign_data_);
     assert(image_.data);
     assert(image_.header.bytesize == size);
     //_free_image();
 
+    /// A bitwise copy of the passed data is created in image_.
     std::copy_n(data, image_.header.bytesize, image_.data);
 }
 
 void tv::ImageAllocator::_free_image(void) {
     LogDebug("IMAGE_ALLOCATOR", "FreeImage for ", id_);
 
+    /// Depending on the value of using_foreign_data_, deletes allocated data
+    /// block
     if (image_.data and not using_foreign_data_) {
         LogDebug("IMAGE_ALLOCATOR", "Deleting data at ", (void*)image_.data,
                  " for ", id_);
         delete[] image_.data;
     }
+    /// and reset the properties of the ImageHeader.
     image_.header.bytesize = 0;
     image_.header.format = ColorSpace::INVALID;
     image_.data = nullptr;
--- src/lib/imaging/image.hh
+++ src/lib/imaging/image.hh
@@ -1,21 +1,29 @@
-/*
-Tinkervision - Vision Library for https://github.com/Tinkerforge/red-brick
-Copyright (C) 2014-2015 philipp.kroos@fh-bielefeld.de
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
+/// \file image.hh
+/// \author philipp.kroos@fh-bielefeld.de
+/// \date 2015
+///
+/// \brief Declaration of the classes \c Image, \c ImageHeader and \c
+/// ImageAllocator.
+///
+/// This file is part of Tinkervision - Vision Library for Tinkerforge Redbrick
+/// \sa https://github.com/Tinkerforge/red-brick
+///
+/// \copyright
+///
+/// This program is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU General Public License
+/// as published by the Free Software Foundation; either version 2
+/// of the License, or (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program; if not, write to the Free Software
+/// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+/// USA.
 
 #ifndef IMAGING_H
 #define IMAGING_H
@@ -27,35 +35,33 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 namespace tv {
 
-/**
- * Supported image formats. The value range per entry is 0-255 for each format,
- * but the number of bytes per pixel differs.
- * - NONE: Can be used by modules that don't want to process images.
- * - INVALID: This would be an error.
- * - YUYV: packed Y'CbCr data, i.e. Y,V and U are stored in the same
- * datablock, structured like:
- * Y00 U00 Y01 V00 Y02 U01 Y03 V01 Y04 ...
- * Y10 U10 Y11 V10 Y12 U11 Y13 V11 Y14 ...
- * ...
- * For each two Y-values there is one U and one V and one pixel is represented
- * by two bytes. So the number of bytes per row equals width*2.
- * This equals YUV422.
- *
- * - YV12: planar Y'CbCr data, i.e. Y,U and V are stored in consecutive
- * datablocks, structured like:
- * Y00 Y01 Y02 ...
- * Y10 Y11 Y12 ...
- * ...
- * U00 U01 U02 ...
- * ...
- * V00 V01 V02 ...
- * ...
- * where the count(U) = count(V) = count(Y)/4, i.e. count(Y) = width * height
- * and the resolution of U and V is halfed vertically and horizontally.
- * - RGB888: One pixel p = 3 byte where p(1) = R, p(2) = G, p(3) = B
- * So the size of one image is height * width * 3.
- * - BGR888: The same as RGB888 but reordered.
- */
+/// Supported image formats. The value range per entry is 0-255 for each format,
+/// but the number of bytes per pixel differs.
+/// - NONE: Can be used by modules that don't want to process images.
+/// - INVALID: This would be an error.
+/// - YUYV: packed Y'CbCr data, i.e. Y,V and U are stored in the same
+/// datablock, structured like:
+/// Y00 U00 Y01 V00 Y02 U01 Y03 V01 Y04 ...
+/// Y10 U10 Y11 V10 Y12 U11 Y13 V11 Y14 ...
+/// ...
+/// For each two Y-values there is one U and one V and one pixel is represented
+/// by two bytes. So the number of bytes per row equals width*2.
+/// This equals YUV422.
+///
+/// - YV12: planar Y'CbCr data, i.e. Y,U and V are stored in consecutive
+/// datablocks, structured like:
+/// Y00 Y01 Y02 ...
+/// Y10 Y11 Y12 ...
+/// ...
+/// U00 U01 U02 ...
+/// ...
+/// V00 V01 V02 ...
+/// ...
+/// where the count(U) = count(V) = count(Y)/4, i.e. count(Y) = width * height
+/// and the resolution of U and V is halfed vertically and horizontally.
+/// - RGB888: One pixel p = 3 byte where p(1) = R, p(2) = G, p(3) = B
+/// So the size of one image is height * width * 3.
+/// - BGR888: The same as RGB888 but reordered.
 enum class ColorSpace : char {
     NONE,
     INVALID,
@@ -67,16 +73,18 @@ enum class ColorSpace : char {
     GRAY
 };
 
-using Clock = std::chrono::steady_clock;
-using Timestamp = Clock::time_point;
-using ImageData = TV_ImageData;
+using Clock = std::chrono::steady_clock;  ///< Clock used for image timestamps.
+using Timestamp = Clock::time_point;      ///< Convenience typedef.
+using ImageData = TV_ImageData;           ///< Convenience typedef.
 
+/// Header for a frame.
 struct ImageHeader {
-    uint16_t width = 0;
-    uint16_t height = 0;
-    size_t bytesize = 0;
-    Timestamp timestamp;
-    ColorSpace format = ColorSpace::INVALID;
+    uint16_t width = 0;                       ///< Framewidth.
+    uint16_t height = 0;                      ///< Frameheight.
+    size_t bytesize = 0;                      ///< Size in bytes.
+    Timestamp timestamp;                      ///< When grabbed.
+    ColorSpace format = ColorSpace::INVALID;  ///< Frame colorspace.
+
     operator bool(void) const {
         return width > 0 and height > 0 and bytesize > 0 and
                format != tv::ColorSpace::INVALID;
@@ -86,44 +94,79 @@ struct ImageHeader {
 bool operator==(ImageHeader const& lhs, ImageHeader const& rhs);
 bool operator!=(ImageHeader const& lhs, ImageHeader const& rhs);
 
+/// Image consists of a header and a data pointer.
+/// The data pointer may be initialized with a new block, but it can also point
+/// to existing data. This is handled in ImageAllocator.
+/// \note Never use this structure directly. Always use the ImageAllocator.
 struct Image {
-    ImageHeader header;
-    ImageData* data = nullptr;
+    ImageHeader header;         ///< Describing the frame.
+    ImageData* data = nullptr;  ///< Frame data.
 };
 
+/// Utility class to manage allocation and sharing of frame data
+/// blocks across different parts of the library.  The semantic means
+/// of this class is described with the methods allocate(),
+/// set_from_image() and copy_data().  To get access to the contained
+/// image, use image().  Allocated data is automatically destroyed
+/// during destruction or, where necessary, during assignment of
+/// different image data.
 class ImageAllocator {
 private:
-    Image image_;
-    size_t image_init_bytesize_{0};
-    bool using_foreign_data_{false};
+    Image image_;                    ///< The managed frame.
+    size_t image_init_bytesize_{0};  ///< Initial bytesize during allocation.
+    bool using_foreign_data_{
+        false};  ///< True if image_ points to other image's data
 
-    std::string const id_;
-    size_t max_size_{0};  ///< Optional size limit if known at initialization.
+    std::string const id_;  ///< Merely of debugging value.
+    size_t max_size_{0};  ///< Currently unused, optional size limit if known at
+    /// initialization.
 
+    /// Internal helper.
     void _free_image(void);
 
 public:
+    /// Standard c'tor.
     explicit ImageAllocator(std::string const& id);
+
+    /// Currently of no value because max_size_ is ignored.
     ImageAllocator(std::string const& id, size_t known_max_size);
 
     ImageAllocator(ImageAllocator const&) = delete;
     ImageAllocator& operator=(ImageAllocator const&) = delete;
 
+    /// D'tor
     ~ImageAllocator(void);
 
+    /// Allocate a new Image.
+    /// \param[in] width Framewidth.
+    /// \param[in] width Frameheight.
+    /// \param[in] bytesize Framesize in bytes.
+    /// \param[in] format Frame color space.
+    /// \param[in] foreign_data If true, no new block of memory will be
+    /// allocated.
     bool allocate(uint16_t width, uint16_t height, size_t bytesize,
                   ColorSpace format, bool foreign_data);
 
+    /// Convenience method forwarding to the more explicit allocate().
+    /// \param[in] header Parameters of the frame to be allocated.
+    /// \param[in] foreign_data If true, no memory will be allocated.
     bool allocate(ImageHeader const& header, bool foreign_data) {
         return allocate(header.width, header.height, header.bytesize,
                         header.format, foreign_data);
     }
 
+    /// Create a reference to another image.
     void set_from_image(Image const& image);
 
+    /// Create a deep copy of image data.
     void copy_data(ImageData const* data, size_t size);
 
+    /// Access the managed image.
+    /// \return image_.
     Image& image(void) { return image_; }
+
+    /// Access the managed image in a const context.
+    /// \return const image_.
     Image const& operator()(void) const { return image_; }
 };
 }
