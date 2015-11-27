d71a4e1c5328a0d3f86a6b5e6e83348638c7b0f7
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-11-27 16:37:18 +0100

    Update on-device test.

--- src/test/red-brick/scripts/availability.py
+++ src/test/red-brick/scripts/availability.py
@@ -1,31 +1,93 @@
+from time import sleep
+
+def check(code, msg):
+    if code != 0:
+        print "Error in context:", msg, ":", code
+        exit(-1)
+
 def run(red):
-    available = red.vision_camera_available()
+    ok = red.vision_camera_available()
+    check(ok, "Available")
 
-    if available == 0: # vision-library returns 0 as value for OK
-        print "TinkerVision is available"
-    else:
-        print "Tinkervision returned error: " + str(available)
-        exit(-1)
+    ok = red.vision_remove_all_modules()
+    check(ok, "RemoveAll")
 
-    ok, user_path = red.vision_lib_user_load_path();
+    ok, user_path = red.vision_lib_get_user_load_path();
+    check(ok, "UserPath")
 
-    if ok != 0:
-        print "Could not get user load path"
-        exit(-1)
-
-    [ok, system_path] = red.vision_lib_system_load_path();
-
-    if ok != 0:
-        print "Could not get system load path"
-        exit(-1)
+    [ok, system_path] = red.vision_lib_get_system_load_path();
+    check(ok, "SystemLoadPath")
 
     print "System module path:", system_path
     print "User module path:", user_path
 
     [ok, count] = red.vision_libs_count();
-
-    if ok != 0:
-        print "Could not get number of available modules"
-        exit(-1)
+    check(ok, "LibsCount")
 
     print "Total available modules:", count
+
+    for i in range(count):
+        [ok, name, path] = red.vision_lib_name_path(i)
+        check(ok, "LibNamePath")
+
+        print "Describing module", path + name
+
+        [ok, p_count] = red.vision_lib_parameters_count(name)
+        check(ok, "ParameterCount")
+
+        print "--", p_count, "parameters"
+
+        for p in range(p_count):
+            [ok, p_name, p_type, p_min, p_max, p_def] = \
+              red.vision_lib_parameter_describe(name, p)
+            check(ok, "ParameterDescribe")
+
+            print "-- Parameter", str(p) + ":", p_name
+            print "-- type:", ("string" if p_type == 1 else "number")
+            if p_type != 1:
+                print "-- min/default/max:", str(p_min) + "/" + str(p_def) + "/" + str(p_max)
+
+    ok = red.vision_start_idle()
+    check(ok, "StartIdle")
+
+    print "Api running in idle mode"
+
+    [ok, width, height] = red.vision_get_framesize()
+    check(ok, "GetFramesize")
+
+    print "Working with framesize", str(width) + "x" + str(height)
+
+    ok = red.vision_set_framesize(320, 240)
+    check(ok, "SetFramesize")
+
+    [ok, width, height] = red.vision_get_framesize()
+    check(ok, "GetFramesize")
+
+    print "Set framesize to", str(width) + "x" + str(height)
+    print "Running for 2 seconds"
+    sleep(2)
+
+    [ok, period] = red.vision_get_frameperiod()
+    check(ok, "GetFrameperiod")
+
+    if period > 0:
+        print "Current framerate:", str(1.0/period)
+
+    inv_rate = 1000
+    print "Setting framerate to", str(1.0/inv_rate)
+    ok = red.vision_request_frameperiod(inv_rate)
+    check(ok, "RequestFrameperiod")
+
+    print "Running for 2 seconds"
+    sleep(2)
+
+    [ok, period] = red.vision_get_frameperiod()
+    check(ok, "GetFrameperiod")
+
+    if period > 0:
+        print "Current framerate:", str(1.0/period)
+
+    ok = red.vision_remove_all_modules()
+    check(ok, "RemoveAll")
+
+    print "Removed all modules, done"
