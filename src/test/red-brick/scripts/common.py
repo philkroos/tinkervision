16bf8cd269051def2f881d30e2538ba516097479
Philipp Kroos <philipp.kroos@fh-bielefeld.de>
2015-12-08 14:29:40 +0100

    Updates on-device test scripts.

--- src/test/red-brick/scripts/common.py
+++ src/test/red-brick/scripts/common.py
@@ -15,10 +15,10 @@ def run(red):
     print "Camera 1 selected: ", ok
 
     sleep(1)
-    ok = red.vision_preselect_framesize(640, 480)
+    ok = red.vision_set_framesize(640, 480)
 
     if ok != 0:
-        raise Exception("vision::PreselectFramesize returned error: " + str(ok))
+        raise Exception("vision::SetFramesize returned error: " + str(ok))
 
     sleep(1)
     ok = red.vision_start_idle()
@@ -29,10 +29,10 @@ def run(red):
     sleep(1)
     width = 0
     height = 0
-    ok, width, height = red.vision_get_resolution()
+    ok, width, height = red.vision_get_framesize()
 
     if ok != 0:
-        raise Exception("vision::GetResolution returned error: " + str(ok))
+        raise Exception("vision::GetFramesize returned error: " + str(ok))
 
     print "Resolution is " + str(width) + "x" + str(height)
 
